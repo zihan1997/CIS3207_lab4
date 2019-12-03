b6e8c6e15a4609c316e62eb952b4a4fad82e0816
zihan1997 <zihan9701@gmail.com>
2019-12-03 12:35:12 -0500

    add prototypes

--- fat.h
+++ fat.h
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <stdint.h>
 #include <limits.h>
+#include <string.h>
 
 
 typedef struct SuperBlock
@@ -69,6 +70,15 @@ static VolumeHeader volumeHeader;
 int make_fs(char* disk_name);
 int mount_fs(char *disk_name);
 int umount_fs(char *disk_name);;
-
+int fs_open(char *name);
+int fs_close(int fildes);
+int fs_create(char *name);
+int fs_delete(char *name);
+int fs_mkdir(char *name);
+int fs_read(int fildes, void *buf, size_t nbyte);
+int fs_write(int fildes, void *buf, size_t nbyte);
+int fs_get_filesize(int fildes);
+int fs_lseek(int fildes, off_t offset);
+int fs_truncate(int fildes, off_t length);
 
 #endif
